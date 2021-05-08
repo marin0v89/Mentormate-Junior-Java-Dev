3e1029aff8a5fcba8f216d4db0a3c506c0cf4dcd
marin0v89 <xandarous89@gmail.com>
2021-05-08 16:23:34 +0300

    Problem solve for junior java dev for Mentormate.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,8 @@
 import Core.Engine;
 
-import java.io.IOException;
-
-
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Engine engine = new Engine();
         engine.run();
     }
