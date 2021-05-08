c8629e8c6841724814088a39e4fa9c2a2f27c2e2
marin0v89 <xandarous89@gmail.com>
2021-05-08 15:44:50 +0300

    Problem solve for junior java dev for Mentormate.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,8 +6,6 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
