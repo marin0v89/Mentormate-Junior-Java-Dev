d5c509d20088394382b253138357f1f3d1a1bb80
marin0v89 <xandarous89@gmail.com>
2021-05-09 10:58:40 +0300

    Problem solve for junior java dev for Mentormate.

---
+++ src/main/java/core/ParseJsonArray.java
@@ -0,0 +1,58 @@
+package core;
+
+import enums.infoData;
+import io.InputReader;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParseJsonArray {
+    private static List<Employee> employees;
+
+    public static void parseArray(JSONParser parser){
+        String path = "src/jsonFile/data.json";
+
+        InputReader reader = new InputReader(path);
+        try {
+            Object obj = parser.parse(reader.readFile());
+            JSONArray employeeList = (JSONArray) obj;
+
+            employees = addEmployeeObj(employeeList);
+    } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+    }
+    private static List<Employee> addEmployeeObj(JSONArray employeeList) {
+        List<Employee> employees = new ArrayList<>();
+        for (Object element : employeeList) {
+
+            JSONObject employeeObject = (JSONObject) element;
+
+            //Reading the employee data from the file
+            String name =
+                    (String) getJSONArray(employeeObject, String.valueOf(infoData.name));
+            Long totalSales =
+                    (Long) getJSONArray(employeeObject, String.valueOf(infoData.totalSales));
+            Long salesPeriod =
+                    (Long) getJSONArray(employeeObject, String.valueOf(infoData.salesPeriod));
+            Double experienceMultiplier =
+                    (Double) getJSONArray(employeeObject, String.valueOf(infoData.experienceMultiplier));
+
+
+            employees.add(new Employee(name, totalSales, salesPeriod, experienceMultiplier));
+        }
+        return employees;
+    }
+    private static Object getJSONArray(JSONObject employeeObject, String predicate) {
+        return employeeObject.get(predicate);
+    }
+
+    public static List<Employee> getEmployees() {
+        return employees;
+    }
+}
