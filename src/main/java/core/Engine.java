8e93b73720edc6aebdc6ad427231f98d5fd4cffb
marin0v89 <xandarous89@gmail.com>
2021-05-09 10:58:36 +0300

    Problem solve for junior java dev for Mentormate.

--- src/main/java/core/Engine.java
+++ src/main/java/core/Engine.java
@@ -1,48 +1,24 @@
 package core;
 
-import enums.infoData;
-import io.InputReader;
 import io.OutputWriter;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
-public class Engine implements Runnable{
+public class Engine implements Runnable {
     @Override
     public void run() {
         //Parsing the JSON file.
         JSONParser parser = new JSONParser();
-        String path = "src/jsonFile/data.json";
 
-        InputReader reader = new InputReader(path);
+        ParseJsonArray.parseArray(parser);
         try {
-            Object obj = parser.parse(reader.readFile());
-            JSONArray employeeList = (JSONArray) obj;
-
-            List<Employee> employees = addEmployeeObj(employeeList);
-
-            //Reading report definition data file
-            path = "src/jsonFile/reportDefinition.json";
-            InputReader reportReader = new InputReader(path);
-
-            Object reportObj = parser.parse(reportReader.readFile());
-            JSONObject dataCompare = (JSONObject) reportObj;
-
-            Long topPerformersThreshold =
-                    (Long) getJSONData(dataCompare, String.valueOf(infoData.topPerformersThreshold));
-            boolean useExprienceMultiplier =
-                    (boolean) getJSONData(dataCompare, String.valueOf(infoData.useExprienceMultiplier));
-            Long periodLimit =
-                    (Long) getJSONData(dataCompare, String.valueOf(infoData.periodLimit));
+            List<Employee> employees = ParseJsonArray.getEmployees();
 
             //Writing to the result file + implementing some of the logic and condition checks
-
-            path = "src/jsonFile/result";
+            ParseJsonObj.parseData(parser);
+            String path = "src/jsonFile/result";
 
             OutputWriter file = new OutputWriter(path);
             file.write("Name , Score");
@@ -54,6 +30,10 @@ public class Engine implements Runnable{
                 long salesPeriod = employee.getSalesPeriod();
                 long totalSales = employee.getTotalSales();
                 double expMultiplier = employee.getExperienceMultiplier();
+
+                long periodLimit = ParseJsonObj.getPeriodLimit();
+                boolean useExprienceMultiplier = ParseJsonObj.isUseExprienceMultiplier();
+                long topPerformersThreshold = ParseJsonObj.getTopPerformersThreshold();
 
                 if (salesPeriod <= periodLimit && useExprienceMultiplier) {
                     double score =
@@ -70,38 +50,8 @@ public class Engine implements Runnable{
                 }
             }
             file.flush();
-        } catch (ParseException | IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
-
-    }
-    private static List<Employee> addEmployeeObj(JSONArray employeeList) {
-        List<Employee> employees = new ArrayList<>();
-        for (Object element : employeeList) {
-
-            JSONObject employeeObject = (JSONObject) element;
-
-            //Reading the employee data from the file
-            String name =
-                    (String) getJSONArray(employeeObject, String.valueOf(infoData.name));
-            Long totalSales =
-                    (Long) getJSONArray(employeeObject, String.valueOf(infoData.totalSales));
-            Long salesPeriod =
-                    (Long) getJSONArray(employeeObject, String.valueOf(infoData.salesPeriod));
-            Double experienceMultiplier =
-                    (Double) getJSONArray(employeeObject, String.valueOf(infoData.experienceMultiplier));
-
-
-            employees.add(new Employee(name, totalSales, salesPeriod, experienceMultiplier));
-        }
-        return employees;
-    }
-
-    private static Object getJSONData(JSONObject dataCompare, String predicate) {
-        return dataCompare.get(predicate);
-    }
-
-    private static Object getJSONArray(JSONObject employeeObject, String predicate) {
-        return employeeObject.get(predicate);
     }
 }
